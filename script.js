0651cc7d3a27688046073b221a014bd2b71026ec
juliaentertainmentservices <juliaentertainmentservices@gmail.com>
2026-07-21 09:27:46 -0400

    Add clock update function to display current time

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+function updateClock() {
+  const now = new Date();
+  const options = {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  };
+
+  const time = now.toLocaleTimeString('en-US', options);
+  const date = now.toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric'
+  });
+
+  document.getElementById('clock').textContent = `${date} | ${time} EST`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
